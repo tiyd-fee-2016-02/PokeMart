6103edb61190c7c5c0b5bfa7b3f905f1b68c542d
Andrew Baron <Drowelfofdead@gmail.com>
2016-03-18 08:28:52 -0400

    add controllers.js and remove manifest testing json

---
+++ assets/js/controllers.js
@@ -0,0 +1,11 @@
+pokeApp.controller('ItemsController', function($scope){
+  $scope.message = "This is the Items Controller"
+});
+
+pokeApp.controller('DetailsController', function($scope){
+  $scope.message = "This is the Items Controller"
+});
+
+pokeApp.controller('CartController', function($scope){
+  $scope.message = "This is the Items Controller"
+});
